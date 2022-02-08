b831a234e25a43981c56a4bb3b05f5da978dbda2
jami6 <98964844+jami6@users.noreply.github.com>
2022-02-08 16:07:25 +0300

    Update main.c
    request activation

--- main.c
+++ main.c
@@ -6,17 +6,21 @@
 #include <mysql.h>
 
 char code[4];
+char query1[255];
+char query2[255];
+char query3[255];
+char query4[255];
+char query5[255];
+char query6[255];
 char sqlquery[255];
-MYSQL_RES* resultset;
+MYSQL_RES *res;
 MYSQL_ROW row;
 MYSQL* conn;
 int k[7][5], com, n, i, j;
 char ID[4];
- char datefrom[12];
- char dateto[12];
- int score=0, choice, temp;
-
-void attemptassignment(){
+char datefrom[12];
+char dateto[12];
+int score=0, choice, rec;
 
     struct alphabet{
 		char letter;
@@ -51,13 +55,26 @@ void attemptassignment(){
     {'Z',{{1,1,1,1,1},{0,0,0,1,0},{0,0,1,0,0},{0,1,0,0,0},{1,0,0,0,0},{1,0,0,0,0},{1,1,1,1,1}} }
  };
 
-    strcpy(sqlquery, "SELECT char1, char2, char3, char4, char5, char6, char7, char8 FROM assignment WHERE assignNo = \'");
-    strcat(sqlquery, ID);
-    strcat(sqlquery,"\'");
-    if(mysql_query(conn,sqlquery)==0){
-    resultset=mysql_use_result(conn);
+ int attemptassignment(){
+    strcpy(query1, "SELECT char1, char2, char3, char4, char5, char6, char7, char8 FROM assignment WHERE assignNo = \'");
+    strcat(query1, ID);
+    strcat(query1,"\'");
+    if(mysql_query(conn,query1)==0){
+    res=mysql_use_result(conn);
+    while((row=mysql_fetch_row(res))!=NULL){
     printf("\nATTEMPT ASSIGNMENT");
-	printf("\n\n");
+	printf("\n");
+
+	char LET[]="0";
+    int a=0;
+    strcpy(LET,row[a]);
+    for(a=1;a<8;a++){
+        if(row[a]!=NULL){
+          strcat(LET,row[a]);
+     }
+    }
+
+    printf("This assignment has %d characters.\n\n", strlen(LET));
 	printf("Guiding grid box\n");
 	for(int x=0;x<7;x++){
 		for(int y=0;y<5;y++){
@@ -67,17 +84,7 @@ void attemptassignment(){
 	}
 	printf("\n");
 	puts("Enter 1 or 0 at each of the co-ordinates");
-    while((row=mysql_fetch_row(resultset))!=NULL){
-    char LET[]="0";
-    int a=0;
 
-    strcpy(LET,row[a]);
-    for(a=1;a<8;a++){
-        if(row[a]!=NULL){
-          strcat(LET,row[a]);
-     }
-    }
-    //printf("%d", strlen(LET));
     for(a=0;a<strlen(LET);a++){
     printf("%c\n",LET[a]);
 
@@ -87,8 +94,7 @@ void attemptassignment(){
         scanf("%d", &k[i][j]);
           for(n=0;n<26;n++){
  	      if(alphabet_array[n].letter==LET[a]){
-            if(alphabet_array[temp].position[i][j]==k[i][j]){
-               //printf("winn");
+            if(alphabet_array[n].position[i][j]==k[i][j]){
                score+=1;
 		 }
 	 }
@@ -97,70 +103,95 @@ void attemptassignment(){
       }
       printf("\n");
 
+      for(i=0;i<7;i++){
+      for(j=0;j<5;j++){
+          if(k[i][j]==1){
+            printf("*");
+          }
+          else if(k[i][j]==0){
+            printf(" ");
+          }
+          else{
+            printf("invalid");
+          }
+      }
+      printf("\n");
+      }
     }
 
-
-            //printf("\n");
-    //mysql_free_result(resultset);
-
-		//}
-
-	 //printf("\n");
-
+      strcpy(sqlquery, "INSERT INTO result(assignNo, Usercode, score) VALUES (\')");
+      strcat(sqlquery, ID);
+      strcat(sqlquery,"\', \'");
+      strcat(sqlquery, code);
+      strcat(sqlquery, "\', \'");
+      strcat(sqlquery, score);
+      strcat(sqlquery,"\')");
+      if(mysql_query(conn,sqlquery)!=0){
+      fprintf(stdout,"Score not updated!");
+      }
+      else{
+      printf("Score updated.");
+      }
     }
-    //}
-}
+    mysql_free_result(res);
+    }
+    return score;
 }
 
 void viewall(){
-    strcpy(sqlquery, "SELECT assignNo, startdate, startTime, endTime, closedate FROM assignment");
-    if(mysql_query(conn,sqlquery)==0){
-    resultset=mysql_use_result(conn);
-    while((row=mysql_fetch_row(resultset))!=NULL){
+    strcpy(query2, "SELECT assignNo, startdate, startTime, endTime, closedate FROM assignment");
+    if(mysql_query(conn,query2)==0){
+    res=mysql_use_result(conn);
+    while((row=mysql_fetch_row(res))!=NULL){
     printf("%s\t%s\t%s\t%s\t%s\n", row[0],row[1],row[2],row[3],row[4]);
     }
-    mysql_free_result(resultset);
+    mysql_free_result(res);
     }
 }
+
 void status();
 
 void assignmentID(){
-    strcpy(sqlquery, "SELECT startdate, startTime, endTime, closedate FROM assignment WHERE assignNo = \'");
-    strcat(sqlquery, ID);
-    strcat(sqlquery,"\'");
-    if(mysql_query(conn,sqlquery)==0){
-    resultset=mysql_use_result(conn);
-    while((row=mysql_fetch_row(resultset))!=NULL){
+    strcpy(query3, "SELECT startdate, startTime, endTime, closedate FROM assignment WHERE assignNo = \'");
+    strcat(query3, ID);
+    strcat(query3,"\'");
+    if(mysql_query(conn,query3)==0){
+    res=mysql_use_result(conn);
+    while((row=mysql_fetch_row(res))!=NULL){
     printf("\n%s\t%s\t%s\t%s\n", row[0],row[1],row[2],row[3]);
     }
-    mysql_free_result(resultset);
+    mysql_free_result(res);
 }
 }
 
 void checkdate(){
-    strcpy(sqlquery, "SELECT assignNo, startdate, startTime, endTime, closedate FROM assignment WHERE startdate BETWEEN \'");
-    strcat(sqlquery, datefrom);
-    strcat(sqlquery,"\'");
-    strcat(sqlquery, "AND \'");
-    strcat(sqlquery, dateto);
-    strcat(sqlquery,"\'");
-    if(mysql_query(conn,sqlquery)==0){
-    resultset=mysql_use_result(conn);
-    while((row=mysql_fetch_row(resultset))!=NULL){
+    strcpy(query4, "SELECT assignNo, startdate, startTime, endTime, closedate FROM assignment WHERE startdate BETWEEN \'");
+    strcat(query4, datefrom);
+    strcat(query4,"\'");
+    strcat(query4, "AND \'");
+    strcat(query4, dateto);
+    strcat(query4,"\'");
+    if(mysql_query(conn,query4)==0){
+    res=mysql_use_result(conn);
+    while((row=mysql_fetch_row(res))!=NULL){
     printf("\n%s\t%s\t%s\t%s\t%s\n", row[0],row[1],row[2],row[3],row[4]);
     }
-    mysql_free_result(resultset);
+    mysql_free_result(res);
     }
 }
 
 void request(){
-    char ACT[]="Requesting activation.";
-    strcpy(sqlquery, "INSERT INTO pupil() VALUES (\'");
-    strcat(sqlquery, ACT);
-    strcat(sqlquery,"\')");
-    if(mysql_query(conn,sqlquery)!=0){
-    fprintf(stderr,"Not sent!");
-    exit(1);
+    char firstname[10];
+    puts("enter your firstname= ");
+    scanf("%s", firstname);
+
+    strcpy(query6, "INSERT INTO request(Usercode, Firstname) VALUES (\'");
+    strcat(query6, code);
+    strcat(query6,"\', \'");
+    strcat(query6, firstname);
+    strcat(query6,"\')");
+    if(mysql_query(conn,query6)!=0){
+    fprintf(stdout,"Not sent!");
     }
     else{
     printf("Request sent.");
@@ -178,20 +209,22 @@ int main()
         printf("Not connected\n");
     }
 
+    do{
     printf("Login with usercode: ");
     scanf("%s", code);
     strcpy(sqlquery, "SELECT Firstname FROM pupil WHERE Usercode = \'");
     strcat(sqlquery, code);
     strcat(sqlquery,"\'");
-    if(mysql_query(conn,sqlquery)!=0){
-    fprintf(stderr, "No match found\n");
-    exit(1);
-    }
-    else{
-        resultset=mysql_use_result(conn);
-        while((row=mysql_fetch_row(resultset))!=NULL){
+    mysql_query(conn,sqlquery);
+    res = mysql_store_result(conn);
+    rec = mysql_num_rows(res);
+    if(rec == 0)
+    fprintf(stdout,"Unknown usercode!\n");
+    }while(rec==0);
+
+        while((row=mysql_fetch_row(res))!=NULL){
             printf("\nWelcome to your account %s.\n", row[0]);
-            mysql_free_result(resultset);
+            mysql_free_result(res);
 
         printf("\n===============DASHBOARD===============\n");
         printf("Command options:\n");
@@ -201,15 +234,16 @@ int main()
         printf("4.View assignment[assignment ID]- see the assignment details of the specified assignment ID.\n");
         printf("5.Checkdates[datefrom dateto]\t- see any assignments within the specified time range.\n");
         printf("6.Requestactivation\t\t- request for activation in case you are deactivated.\n");
+        printf("7.Log out\n");
         do{
         printf("\nEnter command number: ");
         scanf("%d", &com);
-        fgetc(stdin);
         if(com==1){
             printf("Enter assignment ID: ");
             scanf("%s", ID);
             fgetc(stdin);
             attemptassignment();
+            printf("Your Score= %d.", score);
         }
         else if(com==2){
             viewall();
@@ -230,11 +264,17 @@ int main()
         else if(com==6){
             request();
         }
-        printf("\n===============Back to Dashboard(1/0): ");
-        scanf("%d", &choice);
-        }while(choice==1);
-    }
+        else if(com==7){
+            exit(1);
         }
+        else{
+            printf("Invalid option!");
+        }
+
+        printf("\n===============Back to Dashboard(Press 9): ");
+        scanf("%d", &choice);
+        }while(choice==9);
+    }
 
 }
 
