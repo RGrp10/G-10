83388c8ec4a68f7b11442d57efce827433b005e3
abelagaba <77072924+abelagaba@users.noreply.github.com>
2022-02-14 19:08:47 +0300

    assignment

--- kindercare/sidenav.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo 'Dashboard<br><br>
-<a href = "pupils.php">Pupils</a>
-<a href = "setassignment.php">Assignment</a>
-<a href = "results.php">Results</a>
-<a href = "reports.php">Reports</a>'
-?>
