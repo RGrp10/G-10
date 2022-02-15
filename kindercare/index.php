7ae77d2c8c02e94998dd7d2981068c043e2ddf90
abelagaba <77072924+abelagaba@users.noreply.github.com>
2022-02-15 10:38:26 +0300

    welcome

--- kindercare/index.php
+++ kindercare/index.php
@@ -1,37 +1,32 @@
 <?php
     require_once 'includes/header.php';
-	
 ?>
+
 <div class="container">
 	<div class="screen">
 		<div class="screen__content">
-
-<center style="margin-top: 200px;">
-<div class="container">
-<div>
-
-<form class="login" method="post" action="authourize.php">
-				<div class="login__field" >
-					<label for="">Username</label>
-					<input type="text"  name="username" class="form-control" style="width: 250px">
-				</div>
-				<div class="login__field">
-                    <label for="">Password</label>
-					<input type="password"  name="password" class="form-control" style="width: 250px">
-				</div>
-				<button  type="submit" name="logButton" class="btn btn-primary">
-					<span class="button__text">Log In</span>
-				</button>
-                <p>Don't have an account? <a href="register.php">Register!</a></p>				
-			</form>
-</div>
+			<center style="margin-top: 200px;">
+			<div class="container">
+				<p>Welcome to KinderCare Character Draw System. Please register here.</p>
+				<form class="login" method="post" action="authourize.php">
+					<div class="login__field" >
+						<label for="">Username</label>
+						<input type="text"  name="username" class="form-control" style="width: 250px">
+					</div>
+					<div class="login__field">
+						<label for="">Password</label>
+						<input type="password"  name="password" class="form-control" style="width: 250px"><br>
+					</div>
+					<button  type="submit" name="logButton" class="btn btn-primary">
+						<span class="button__text">Log In</span>
+					</button>
+					<p>Don't have an account? <a href="register.php">Register!</a></p>				
+				</form>
+			</div>
 		</div>
+	</div>
+</div>
 </center>
-		
-			
-
-			
-
 
 <?php
     require_once 'includes/footer.php';
