aa43d8f2435737bc7562fa3b3e9014d955e9b2f5
Don Kaijuka <donald.kaijuka1@gmail.com>
2022-02-16 18:02:04 +0300

    Update index.php

--- kindercare/index.php
+++ kindercare/index.php
@@ -1,32 +1,39 @@
 <?php
     require_once 'includes/header.php';
+	
 ?>
 
-<div class="container">
+<div class="container"  >
 	<div class="screen">
 		<div class="screen__content">
-			<center style="margin-top: 200px;">
-			<div class="container">
-				<p>Welcome to KinderCare Character Draw System. Please register here.</p>
-				<form class="login" method="post" action="authourize.php">
-					<div class="login__field" >
-						<label for="">Username</label>
-						<input type="text"  name="username" class="form-control" style="width: 250px">
-					</div>
-					<div class="login__field">
-						<label for="">Password</label>
-						<input type="password"  name="password" class="form-control" style="width: 250px"><br>
-					</div>
-					<button  type="submit" name="logButton" class="btn btn-primary">
-						<span class="button__text">Log In</span>
-					</button>
-					<p>Don't have an account? <a href="register.php">Register!</a></p>				
-				</form>
-			</div>
-		</div>
-	</div>
+
+<center style="margin-top: 200px;">
+<div class="container">
+	<h1>KINDERCARE CHARACTER DRAW</h1>
+<div>
+
+<form class="login-dark" method="post" action="authourize.php">
+				<div class="login__field" >
+					<label for="">Username</label>
+					<input type="text"  name="username" class="form-control" style="width: 250px">
+				</div>
+				<div class="login__field">
+                    <label for="">Password</label>
+					<input type="password"  name="password" class="form-control" style="width: 250px">
+				</div>
+				
+				<p style="text-align: center;"><button type="submit" class="btn btn-dark btn-sm" name="logButton" >
+                                <i class="fa fa-dot-circle-o"></i>Log In
+                            </button></p>
+                <p>Don't have an account? <a href="register.php">Register!</a></p>				
+			</form>
 </div>
+		</div>
 </center>
+		
+			
+			
+
 
 <?php
     require_once 'includes/footer.php';
